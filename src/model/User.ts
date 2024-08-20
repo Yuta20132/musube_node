88b728eecf5eae52a767040836087942b001fd66
yamma10 <yamtt1326@gmail.com>
2024-08-20 19:05:59 +0900

    登録のクエリ作成

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,31 +1,46 @@
-export class User {
+export class user_registration {
+  constructor(
+    name: string,
+    first_name: string,
+    last_name: string,
+    category_id: number,
+    email: string,
+    password: string) {
+    this.name = name;
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.category_id = category_id;
+    this.email = email;
+    this.password = password;
+    }
+  name: string;
+  first_name: string;
+  last_name: string;
+  category_id: number;
+  email: string;
+  password: string;
+}
+
+export class User extends user_registration {
   constructor(id: number, 
     name: string, 
-    firstname: string,
-    lastname: string,
+    first_name: string,
+    last_name: string,
     email: string,
+    category_id: number,
     is_active: boolean, 
     is_admin: boolean,
     password: string,
     created_at: Date) {
+    super(name, first_name, last_name, category_id, email, password);
     this.id = id;
-    this.name = name;
-    this.firstname = firstname;
-    this.lastname = lastname;
-    this.email = email;
     this.is_active = is_active;
     this.is_admin = is_admin;
-    this.password = password;
     this.created_at = created_at;
-  }
 
-  id: number;
-  name: string;
-  firstname: string;
-  lastname: string;
-  email: string;
+  }
+  id: number
   is_active: boolean;
   is_admin: boolean;
-  password: string;
   created_at: Date;
 }
