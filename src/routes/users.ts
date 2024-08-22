69ef8d4f996c563957e7f69c0c8badcc71c3aae9
yamma10 <yamtt1326@gmail.com>
2024-08-22 18:09:46 +0900

    パスワードをハッシュ化

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { user_registration } from "../model/User";
 import { createRegistrationQuery } from "../components/createQuery";
 import client from "../db/client";
+import { hashPassword } from "../components/hashUtils";
 
 
 const router = express.Router();
@@ -15,24 +16,30 @@ router.post("/", async(req, res) => {
   console.log(req.body);
   const { user_name, first_name, last_name, category_id, email, password } = req.body;
 
-
-  const user: user_registration = {
-    user_name: user_name,
-    first_name: first_name,
-    last_name: last_name,
-    category_id: category_id,
-    email: email,
-    password: password
-  }
-
-  
-  const query: string = createRegistrationQuery(user);
-  console.log(query);
-
   try {
+    //パスワードをハッシュ化
+    const hasyPassword = await hashPassword(password);
+
+    //ユーザー情報を作成
+    const user: user_registration = {
+      user_name: user_name,
+      first_name: first_name,
+      last_name: last_name,
+      category_id: category_id,
+      email: email,
+      password: hasyPassword
+    }
+
+    //ユーザー情報を登録するクエリを作成
+    const query: string = createRegistrationQuery(user);
+
+    console.log(query);
+
     //データベースに接続
     await client.connect();
     console.log("connected")
+    
+    //ユーザー情報をデータベースに登録
     const result = await client.query(query);
     console.log(result.rows[0])
     res.send("User registered");
