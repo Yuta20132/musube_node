932cfb1e1d45c6047f64e735088b85367f9f65cd
yutaxxx <yuta2013docomo@gmail.com>
2024-08-27 21:17:38 +0900

    corsの追加

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import express from 'express';
 import http from 'http';
 import { getCurrentTime } from './services/timeService';
 import userRoute from './routes/users';
+import cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
 
 const WS_PORT = 8080;
 
+// カスタムCORS設定
+const corsOptions = {
+  origin: '*', // 許可するオリジン
+  methods: 'GET,POST', // 許可するHTTPメソッド
+  allowedHeaders: 'Content-Type,Authorization' // 許可するヘッダー
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
