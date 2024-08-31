8532ef4faa64af64b345595b883daf4a2aafb8f0
yamma10 <yamtt1326@gmail.com>
2024-08-31 16:55:54 +0900

    スレッド登録の際のモデル追加

--- src/model/Threads.ts
+++ src/model/Threads.ts
@@ -1,3 +1,19 @@
+export class thread_registration {
+  constructor(
+    title: string,
+    description: string,
+    category_id: number,
+  ) {
+    this.title = title;
+    this.description = description;
+    this.category_id = category_id;
+}
+
+  title: string;
+  description: string;
+  category_id: number;
+
+}
 export class Thread {
   constructor(
     id: number,
