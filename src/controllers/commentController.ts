f6e7e564f3722e25999ec9a506a18a73e66d0be0
yamma10 <yamtt1326@gmail.com>
2024-10-26 10:00:56 +0900

    コメント機能の作成

---
+++ src/controllers/commentController.ts
@@ -0,0 +1,52 @@
+import { createAdminCommentQuery, createCommentQuery } from "../components/createQuery";
+import pool from "../db/client";
+
+export const CommentCreateController = async (post_id: number, user_id: string, content: string, category_id: number) => {
+  let client;
+
+  try {
+    client = await pool.connect();
+
+    console.log("コメントを投稿するクエリの作成");
+    if(category_id === 5) {
+      const query = createAdminCommentQuery();
+      const result = await client.query(query, [post_id, user_id, content]);
+      //もしresult.rowCountが0だったらエラーを返す
+      if (result.rowCount === 0) {
+        console.dir(result, { depth: null });
+        console.log("コメントの投稿に失敗");
+        throw new Error("コメントの投稿に失敗しました");
+      } else {
+        console.log("コメントの投稿に成功");
+        //console.log(result.rowCount);
+      }
+    } else {
+      const query = createCommentQuery();
+      const result = await client.query(query, [post_id, category_id, user_id, content]);
+      
+      if (result.rowCount === 0) {
+        console.log(result.rowCount);
+        console.log("コメントの投稿に失敗");
+        throw new Error("post_idとcategory_idの組み合わせが異なるためコメントに失敗しました");
+      } else {
+        console.log("コメントの投稿に成功");
+        //console.log(result.rowCount);
+      }
+    }
+    
+
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("コメント投稿エラー", error);
+      throw new Error("エラーが発生");
+    } else {
+      console.log("予期しないエラー: ", error)
+      throw new Error("何らかのエラーが発生");
+    }
+  } finally {
+    if (client) {
+      client.release();
+    }
+    console.log("disconnected\n");
+  }
+}
---
+++ src/routes/comments.ts
@@ -0,0 +1,71 @@
+import express from 'express';
+import { getPayloadFromJWT, verifyJWT } from '../components/jwt';
+import { CommentCreateController } from '../controllers/commentController';
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("All comments");
+});
+
+//post_id, content, category_idを受け取ってコメントを作成する
+//commentControllerに送るのはpost_idとcontent, payloadから取得したuser_idを送る
+router.post("/", async (req, res) => {
+  const { post_id, category_id, content } = req.body;
+  //post_id, contentがない場合はエラーを返す
+  if (post_id === undefined || content === undefined || category_id === undefined) {
+    res.status(400).send(`${post_id === undefined ? "post_id " : ""}${category_id === undefined ? "category_id " : ""}${content === undefined ? "content" : ""}が入力されていません`);
+    return;
+  }
+
+  //トークンがあるか確認
+  const token = req.cookies.bulletin_token;
+  if (token === undefined || token === null) {
+    res.status(400).send("トークンがありません");
+    return;
+  }
+
+  //トークンの検証
+  if (!verifyJWT(token)) {
+    res.status(400).send("トークンが無効です");
+    return;
+  }
+
+  //トークンから情報を取得
+  const payload = getPayloadFromJWT(token);
+  if (payload === null) {
+    res.status(400).send("トークンの解析に失敗しました");
+    return;
+  }
+
+  //payloadにあるユーザのcategory_idと受け取った投稿のcategory_idが一致しない場合エラーを返す
+  if (payload.category_id !== category_id) {
+    res.status(400).send("権限がありません");
+    return;
+  }
+
+  //thread_idを数値に変換できるか確認
+  if (isNaN(Number(post_id))) {
+    res.status(400).send("post_idが数値ではありません");
+    return;
+  } 
+  
+  //category_idを数値に変換できるか確認
+  if (isNaN(Number(category_id))) {
+    res.status(400).send("category_idが数値ではありません");
+    return;
+  }
+
+  try {
+    //コメントの作成
+    await CommentCreateController(Number(post_id), payload.user_id, content, Number(category_id));
+    res.status(200).send("コメントの作成に成功しました");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("コメントの作成に失敗しました");
+  }
+
+
+});
+
+export default router;
