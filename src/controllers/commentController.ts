10672197f15874d008349bf60c1dc174db43e005
yamma10 <yamtt1326@gmail.com>
2024-11-20 23:37:46 +0900

    コメントの作成と削除のバグ修正

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,38 +1,46 @@
 import { createAdminCommentQuery, createCommentQuery } from "../components/createQuery";
 import pool from "../db/client";
 
-export const CommentCreateController = async (post_id: number, user_id: string, content: string, category_id: number) => {
+export const CommentCreateController = async (post_id: number, user_id: string, content: string) => {
   let client;
 
   try {
     client = await pool.connect();
 
     console.log("コメントを投稿するクエリの作成");
-    if(category_id === 5) {
-      const query = createAdminCommentQuery();
-      const result = await client.query(query, [post_id, user_id, content]);
-      //もしresult.rowCountが0だったらエラーを返す
-      if (result.rowCount === 0) {
-        console.dir(result, { depth: null });
-        console.log("コメントの投稿に失敗");
-        throw new Error("コメントの投稿に失敗しました");
-      } else {
-        console.log("コメントの投稿に成功");
-        //console.log(result.rowCount);
-      }
+    const query = createCommentQuery();
+    const result = await client.query(query, [post_id, user_id, content]);
+    if (result.rowCount === 0) {
+      console.log("コメントの投稿に失敗");
+      throw new Error("コメントの投稿に失敗しました");
     } else {
-      const query = createCommentQuery();
-      const result = await client.query(query, [post_id, category_id, user_id, content]);
-      
-      if (result.rowCount === 0) {
-        console.log(result.rowCount);
-        console.log("コメントの投稿に失敗");
-        throw new Error("post_idとcategory_idの組み合わせが異なるためコメントに失敗しました");
-      } else {
-        console.log("コメントの投稿に成功");
-        //console.log(result.rowCount);
-      }
+      console.log("コメントの投稿に成功");
     }
+    // if(category_id === 5) {
+    //   const query = createAdminCommentQuery();
+    //   const result = await client.query(query, [post_id, user_id, content]);
+    //   //もしresult.rowCountが0だったらエラーを返す
+    //   if (result.rowCount === 0) {
+    //     console.dir(result, { depth: null });
+    //     console.log("コメントの投稿に失敗");
+    //     throw new Error("コメントの投稿に失敗しました");
+    //   } else {
+    //     console.log("コメントの投稿に成功");
+    //     //console.log(result.rowCount);
+    //   }
+    // } else {
+    //   const query = createCommentQuery();
+    //   const result = await client.query(query, [post_id, user_id, content]);
+      
+    //   if (result.rowCount === 0) {
+    //     console.log(result.rowCount);
+    //     console.log("コメントの投稿に失敗");
+    //     throw new Error("post_idとcategory_idの組み合わせが異なるためコメントに失敗しました");
+    //   } else {
+    //     console.log("コメントの投稿に成功");
+    //     //console.log(result.rowCount);
+    //   }
+    // }
     
 
   } catch (error) {
@@ -48,5 +56,35 @@ export const CommentCreateController = async (post_id: number, user_id: string, 
       client.release();
     }
     console.log("disconnected\n");
+  }
+}
+
+export const CommentDeleteController = async (comment_id: number) => {
+  let client;
+
+  try {
+    client = await pool.connect();
+
+    const query = `
+      DELETE FROM comments WHERE id = $1;
+    `;
+    const result = await client.query(query, [comment_id]);
+
+    console.dir(result, { depth: null });
+
+    if (result.rowCount === 0) {
+      console.log("コメントの削除に失敗");
+      throw new Error("コメントの削除に失敗しました");
+    } else {
+      console.log("コメントの削除に成功");
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log("コメント削除エラー", error);
+      throw new Error("エラーが発生");
+    } else {
+      console.log("予期しないエラー: ", error)
+      throw new Error("何らかのエラーが発生");
+    }
   }
 }
--- src/routes/comments.ts
+++ src/routes/comments.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getPayloadFromJWT, verifyJWT } from '../components/jwt';
-import { CommentCreateController } from '../controllers/commentController';
+import { CommentCreateController, CommentDeleteController } from '../controllers/commentController';
+import e from 'express';
 
 const router = express.Router();
 
@@ -38,8 +39,8 @@ router.post("/", async (req, res) => {
     return;
   }
 
-  //payloadにあるユーザのcategory_idと受け取った投稿のcategory_idが一致しない場合エラーを返す
-  if (payload.category_id !== category_id) {
+  //payloadにあるユーザのcategory_idと受け取った投稿のcategory_idが一致しないか、payloadのcategory_idが5(管理者)でない場合はエラーを返す
+  if (Number(payload.category_id) !== 5 && Number(payload.category_id) !== Number(category_id)) {
     res.status(400).send("権限がありません");
     return;
   }
@@ -58,7 +59,7 @@ router.post("/", async (req, res) => {
 
   try {
     //コメントの作成
-    await CommentCreateController(Number(post_id), payload.user_id, content, Number(category_id));
+    await CommentCreateController(Number(post_id), payload.user_id, content);
     res.status(200).send("コメントの作成に成功しました");
   } catch (err) {
     console.log(err);
@@ -66,6 +67,60 @@ router.post("/", async (req, res) => {
   }
 
 
+});
+
+//コメントの削除
+//comment_idとuser_idを受け取り、自身のトークンのuser_idと一致するか確認
+//使うかわからん
+router.delete("/", async(req, res) => {
+  //req.bodyからcomment_idとuser_idを取得
+  const { comment_id, user_id } = req.body;
+  //comment_id, user_idがない場合はエラーを返す
+  if (comment_id === undefined || user_id === undefined) {
+    res.status(400).send(`${comment_id === undefined ? "comment_id " : ""}${user_id === undefined ? "user_id" : ""}が入力されていません`);
+    return;
+  }
+
+  //トークンがあるか確認
+  const token = req.cookies.bulletin_token;
+  if (token === undefined || token === null) {
+    res.status(400).send("トークンがありません");
+    return;
+  }
+  //トークンの検証
+  if (!verifyJWT(token)) {
+    res.status(400).send("トークンが無効です");
+    return;
+  }
+  //トークンから情報を取得
+  const payload = getPayloadFromJWT(token);
+  if (payload === null) {
+    res.status(400).send("トークンの解析に失敗しました");
+    return;
+  }
+  //payloadのuser_idと受け取ったuser_idが一致しない場合はエラーを返す
+  if (payload.user_id !== user_id) {
+    res.status(400).send("権限がありません");
+    return;
+  }
+  //comment_idを数値に変換できるか確認
+  if (isNaN(Number(comment_id))) {
+    res.status(400).send("comment_idが数値ではありません");
+    return;
+  }
+
+  //コメントの削除処理
+  try {
+
+    //コメントの削除
+    await CommentDeleteController(Number(comment_id));
+    res.status(200).send("コメントの削除に成功しました");
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err);
+      res.status(500).send("コメントの削除に失敗しました");
+    }
+  }
 });
 
 export default router;
