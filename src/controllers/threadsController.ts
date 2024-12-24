727643a277067e114fbcc17aefacc139f88fdbe4
yamma10 <yamtt1326@gmail.com>
2024-12-24 13:27:09 +0900

    スレッドIDを指定して投稿を取得する機能を実装

--- src/controllers/threadsController.ts
+++ src/controllers/threadsController.ts
@@ -1,7 +1,7 @@
 import { get } from "http";
-import { createGetCategoryQuery, createGetThreadsQuery, createThreadQuery } from "../components/createQuery";
+import {  createGetCategoryQuery, CreateGetPostsByThreadIdQuery, createGetThreadInfoQuery, createGetThreadsQuery, createThreadQuery } from "../components/createQuery";
 import pool from "../db/client";
-import { Thread, thread_registration } from "../model/Threads";
+import { getPostsRequest, getPostsResponse, Thread, thread_info, thread_registration } from "../model/Threads";
 import { user_login } from "../model/User";
 
 
@@ -79,6 +79,90 @@ export const ThreadGetController = async(category_id:string):Promise<Thread[]> =
 
     const threads:Thread[] = result.rows;
     return threads;
+  } catch(error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("なんらかのエラーが発生しました" + error);
+    }
+  } finally {
+    //データベースとの接続を切断
+    if (client) {
+      client.release();
+    }
+    console.log("disconnected\n");
+  }
+}
+
+export const GetPostsByThreadIdController = async(req_params:getPostsRequest): Promise<getPostsResponse> => {
+
+  let client;
+  try {
+    //データベースに接続
+    client = await pool.connect();
+    console.log("connected");
+
+    const query:string = CreateGetPostsByThreadIdQuery();
+    console.log("ポストを取得するクエリ\n");
+    console.log(query);
+
+    const result = await client.query(query, [req_params.thread_id, req_params.limit, req_params.offset]);
+    console.log("ポストを取得");
+    console.log(result.rows);
+
+    //result.rowCountsがない場合はエラーを返す
+    if (result.rowCount === 0) {
+      throw new Error("rowCountが存在しません");
+    }
+
+    const posts:getPostsResponse = {
+      thread_id: req_params.thread_id,
+      thread_title: result.rows[0].thread_title,
+      thread_description: result.rows[0].thread_description,
+      rowCounts: Number(result.rowCount),
+      rows: result.rows,
+    };
+    
+    return posts;
+  } catch(error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    } else {
+      throw new Error("なんらかのエラーが発生しました" + error);
+    }
+  } finally {
+    //データベースとの接続を切断
+    if (client) {
+      client.release();
+    }
+    console.log("disconnected\n");
+  }
+
+}
+
+export const ThreadCategoryGetController = async(thread_id:number):Promise<thread_info> => {
+
+  let client;
+  try {
+    //データベースに接続
+    client = await pool.connect();
+    console.log("connected");
+
+    const query:string = createGetThreadInfoQuery();
+    console.log("スレッドのカテゴリを取得するクエリ\n");
+    console.log(query);
+
+    const result = await client.query(query, [thread_id]);
+    console.log("スレッドのカテゴリを取得");
+    console.log(result.rows);
+
+    if (result.rowCount === 0) {
+      throw new Error("指定されたスレッドは存在しません");
+    }
+
+    const tInfo: thread_info = result.rows[0];
+
+    return tInfo;
   } catch(error) {
     if (error instanceof Error) {
       throw new Error(error.message);
